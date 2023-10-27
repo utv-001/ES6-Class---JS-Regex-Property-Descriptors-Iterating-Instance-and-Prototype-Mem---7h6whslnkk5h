513008bca3b85b4d432f0d1b41030de6a9ad0873
unknown <trivikramunnam@gmail.com>
2023-10-27 16:51:36 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 //Write your code here
-
-
-
-
-
+class API{
+    constructor(url, method = 'GET'){
+        this.url = url;
+        this.method = method;
+        this.#secure = url.startsWith('https');
+    }
+    isSecure(){
+        return this.#secure;
+    }
+    updateUrl(url){
+        this.url = url;
+        this.#secure = url.startsWith("https");
+    }
+}
 //Do not remove this code
 module.exports = { API }
